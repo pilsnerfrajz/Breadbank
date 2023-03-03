d788847ccadd8b2e2ca6ebaa1e8408e2d07ee775
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2023-03-03 19:52:33 +0100

    Create TCPClient.java

---
+++ src/TCPClient.java
@@ -0,0 +1,12 @@
+import java.net.*;
+import java.io.*;
+
+public class TCPClient {
+	public TCPClient(){
+	}
+
+	public byte[] request(String hostname, int port, byte[] toServer){
+		
+		return null;
+	}
+}
