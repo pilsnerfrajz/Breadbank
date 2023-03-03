007c7c9a9dda740eba52933a5a2eee0f65924afb
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2023-03-03 19:57:30 +0100

    Add method to call server

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -5,8 +5,30 @@ public class TCPClient {
 	public TCPClient(){
 	}
 
-	public byte[] request(String hostname, int port, byte[] toServer){
-		
-		return null;
+	public byte[] query(String hostname, int port, byte[] toServer) throws IOException {
+		// create static and dynamic buffers for storing data from server
+		byte[] staticServerBuffer = new byte[1024];
+		ByteArrayOutputStream dynamicServerBuffer = new ByteArrayOutputStream();
+
+		// create socket and connect to server "hostname" on port "port"
+		Socket clientSocket = new Socket(hostname, port);
+
+		// send data to server
+		clientSocket.getOutputStream().write(toServer);
+
+		// store data from server. When no more data is received, read() returns -1
+		while (true) {
+			int lenServer = clientSocket.getInputStream().read(staticServerBuffer);
+			if(lenServer != -1)
+				dynamicServerBuffer.write(staticServerBuffer, 0, lenServer);
+			else
+				break;
+		}
+
+		// close socket
+		clientSocket.close();
+
+		// return data from server
+		return dynamicServerBuffer.toByteArray();
 	}
 }
