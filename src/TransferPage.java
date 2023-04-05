ce9ea734092c2cbb5c6e29450c5f12da7bd7f646
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2023-04-05 20:26:37 +0200

    Add combobox to show account info

--- src/TransferPage.java
+++ src/TransferPage.java
@@ -9,10 +9,11 @@ public class TransferPage extends JFrame  {
 	private JLabel transferAmount;
 	private JTextField acc2Field;
 	private JLabel acc2Label;
-	private JTextField acc1Field;
+	//private JTextField acc1Field;
+	private JComboBox accountList;
 	private JLabel acc1Label;
 
-	public TransferPage(){
+	public TransferPage(String[] accountInfo){
 		this.setSize(400, 400);
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
@@ -25,16 +26,23 @@ public class TransferPage extends JFrame  {
 		acc1Label.setBounds(10, 10, 150, 25);
 		panel.add(acc1Label);
 
-		acc1Field = new JTextField();
-		acc1Field.setBounds(100, 10, 150, 25);
-		panel.add(acc1Field);
+		String[] accountToComboBox = new String[accountInfo.length / 2];
+		int k = 0;
+		for(int i = 0; i < accountToComboBox.length; i++) {
+			accountToComboBox[i] = accountInfo[k] + " ($" + accountInfo[k + 1] + ")";
+			k = k + 2;
+		}
+
+		accountList = new JComboBox(accountToComboBox);
+		accountList.setBounds(100, 10, 200, 25);
+		panel.add(accountList);
 
 		acc2Label = new JLabel("To account");
 		acc2Label.setBounds(10, 50, 150, 25);
 		panel.add(acc2Label);
 
 		acc2Field = new JTextField();
-		acc2Field.setBounds(100, 50, 150, 25);
+		acc2Field.setBounds(100, 50, 200, 25);
 		panel.add(acc2Field);
 
 		transferAmount = new JLabel("Amount");
@@ -42,7 +50,7 @@ public class TransferPage extends JFrame  {
 		panel.add(transferAmount);
 
 		transferAmountField = new JTextField();
-		transferAmountField.setBounds(100, 90, 150, 25);
+		transferAmountField.setBounds(100, 90, 200, 25);
 		panel.add(transferAmountField);
 
 		transferButton = new JButton("Transfer");
@@ -52,17 +60,19 @@ public class TransferPage extends JFrame  {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
-					String acc1 = acc1Field.getText();
+					int comboIndex = accountList.getSelectedIndex();
+					String acc1 = accountInfo[comboIndex + 2];
 					String acc2 = acc2Field.getText();
 					double amount = Double.parseDouble(transferAmountField.getText());
 
 					TCPClient tcpClient = new TCPClient();
-					String transferString = "UPDATE account SET balance=balance + -" + amount + " WHERE acc_no='" + acc1 + "';"
-							+ "UPDATE account SET balance=balance + " + amount + " WHERE acc_no='" + acc2 + "';";
+					String transferString = "TRANSFER UPDATE account SET balance=balance - " + amount + " WHERE acc_no='" + acc1 + "';"
+							+ "UPDATE account SET balance=balance + " + amount + " WHERE acc_no='" + acc2 + "';\0";
 					byte[] toServer = transferString.getBytes();
-					byte[] serverBytes = tcpClient.query("192.168.1.100", 1337, toServer);
+					byte[] serverBytes = tcpClient.query("LOCALHOST", 1337, toServer);
 					String serverOutput = new String(serverBytes);
 					System.out.println(serverOutput);
+					transferButton.setText("Sent!");
 				} catch (IOException ex) {
 					throw new RuntimeException(ex);
 				}
