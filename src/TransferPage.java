0098d2c0dc6ea4a0071b32d892a4bae45e8e2e01
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2023-04-21 16:03:57 +0200

    Add combobox for account info

--- src/TransferPage.java
+++ src/TransferPage.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class TransferPage extends JFrame  {
 	private JTextField transferAmountField;
@@ -9,9 +10,9 @@ public class TransferPage extends JFrame  {
 	private JLabel transferAmount;
 	private JTextField acc2Field;
 	private JLabel acc2Label;
-	//private JTextField acc1Field;
 	private JComboBox accountList;
 	private JLabel acc1Label;
+	private String[] accountToComboBox;
 
 	public TransferPage(String[] accountInfo){
 		this.setSize(400, 400);
@@ -26,16 +27,17 @@ public class TransferPage extends JFrame  {
 		acc1Label.setBounds(10, 10, 150, 25);
 		panel.add(acc1Label);
 
-		String[] accountToComboBox = new String[accountInfo.length / 2];
+		accountToComboBox = new String[accountInfo.length / 2];
 		int k = 0;
 		for(int i = 0; i < accountToComboBox.length; i++) {
-			accountToComboBox[i] = accountInfo[k] + " ($" + accountInfo[k + 1] + ")";
+			accountToComboBox[i] = accountInfo[k] + " (" + accountInfo[k + 1] + " SEK)";
 			k = k + 2;
 		}
 
-		accountList = new JComboBox(accountToComboBox);
+		accountList = new JComboBox<>(accountToComboBox);
 		accountList.setBounds(100, 10, 200, 25);
 		panel.add(accountList);
+
 
 		acc2Label = new JLabel("To account");
 		acc2Label.setBounds(10, 50, 150, 25);
@@ -61,17 +63,22 @@ public class TransferPage extends JFrame  {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					int comboIndex = accountList.getSelectedIndex();
-					String acc1 = accountInfo[comboIndex + 2];
+					String acc1;
+					if(comboIndex == 0)
+						acc1 = accountInfo[comboIndex];
+					else
+						acc1 = accountInfo[comboIndex * 2];
 					String acc2 = acc2Field.getText();
 					double amount = Double.parseDouble(transferAmountField.getText());
 
 					TCPClient tcpClient = new TCPClient();
-					String transferString = "TRANSFER UPDATE account SET balance=balance - " + amount + " WHERE acc_no='" + acc1 + "';"
-							+ "UPDATE account SET balance=balance + " + amount + " WHERE acc_no='" + acc2 + "';\0";
+					String transferString = "TRANSFER UPDATE account SET balance=balance-" + amount + " WHERE account_number='" + acc1 + "';"
+							+ "UPDATE account SET balance=balance+" + amount + " WHERE account_number='" + acc2 + "';\0";
 					byte[] toServer = transferString.getBytes();
 					byte[] serverBytes = tcpClient.query("LOCALHOST", 1337, toServer);
 					String serverOutput = new String(serverBytes);
 					System.out.println(serverOutput);
+					setComboBox(serverOutput);
 					transferButton.setText("Sent!");
 				} catch (IOException ex) {
 					throw new RuntimeException(ex);
@@ -80,25 +87,17 @@ public class TransferPage extends JFrame  {
 		});
 		setVisible(true);
 	}
-/*
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		try {
-			String acc1 = acc1Field.getText();
-			String acc2 = acc2Field.getText();
-			double amount = Double.parseDouble(transferAmountField.getText());
 
-			TCPClient tcpClient = new TCPClient();
-			String transferString = "UPDATE account SET balance=balance + -" + amount + " WHERE acc_no='" + acc1 + "';"
-								+ "UPDATE account SET balance=balance + " + amount + " WHERE acc_no='" + acc2 + "';";
-			byte[] toServer = transferString.getBytes();
-			byte[] serverBytes = tcpClient.query("192.168.1.100", 1337, toServer);
-			String serverOutput = new String(serverBytes);
-			System.out.println(serverOutput);
-		} catch (IOException ex) {
-			throw new RuntimeException(ex);
+	public void setComboBox(String resultServer){
+		String[] accountInfo = resultServer.split(" ");
+		accountList.removeAllItems();
+		int k = 0;
+		for(int i = 0; i < accountToComboBox.length; i++) {
+			accountList.addItem(accountInfo[k] + " (" + accountInfo[k + 1] + " SEK)");
+			k = k + 2;
 		}
+		System.out.println(Arrays.toString(accountToComboBox));
+		this.revalidate();
+		this.repaint();
 	}
-
- */
 }
